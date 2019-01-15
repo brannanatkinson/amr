6f0497a827505b7eb589cdd85122566be3cdfa1c
Brannan Atkinson <brannan@amycommuniations.com>
2019-01-14 20:50:09 -0600

    fixed media page

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -15,27 +15,42 @@
 
 	<div class="row">
 		<div class="sixteen wide columns">
-
-
-
 			@foreach ($outlets as $outlet)
-
-
-				<div class="media__name"><h3>{{ $outlet->org_name }}</h3> <span class="media__count">~ Mentions: {{ $outlet->stories->count() }} ~</span></div> 
+				<hr>
+				<h2 style="font-weight: bolder; text-align: center; padding-top: 20px;">{{ $outlet->org_name }}</h2> 
+				<div style="text-align: center; margin-top: -10px; padding-bottom: 20px;" class="media__count">~ Mentions: {{ $outlet->stories->count() }} ~</div> 
 
 				@if ($outlet->stories->count() > 0)
 
 					<div class="row small-up-2 medium-up-3 large-up-4">
-
+                        <div class="ui items">
 						@foreach ($outlet->stories as $story)
-
-		  					<div class="column column-block">
-
-								@include('media.story_card')
-
+							<div class="item">
+							    <div class="image">
+							        @if(strpos($story->story_url, 'twitter') == true )
+							    	    <img style="height: 100px; object-fit: cover;" src="https://placehold.it/600/1CA1F2/ffffff" alt="" >
+							    	@else
+								        @if ( is_null($story->story_image) )
+											<img style="height: 100px; object-fit: cover;" src="{{ url ('public/img/' . $story->id . '.jpg') }}" alt="">																			
+									    @else
+											<img style="height: 100px; object-fit: cover;" src="{{ $story->story_image }}" alt="" >
+										{{-- @elseif(strpos($story->story_url, 'twitter') == true )
+											<h3>placeholder:twitter</h3> --}}
+										@endif
+						            @endif
+							    </div>
+							    <div class="content">
+							        <a class="header">{{ $story->headline() }}</a>
+							        <div class="meta">
+							            <span>Description</span>
+							        </div>
+								    <div class="extra">
+								        Additional Details
+								    </div>
+							    </div>
 							</div>
-
 						@endforeach
+						</div>
 
 
 					</div>
