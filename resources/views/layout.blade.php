b19d250cc8facb42294518421083bdec96f4c07f
Brannan Atkinson <brannan@amycommuniations.com>
2019-02-12 17:28:07 -0600

    issue with layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -46,9 +46,9 @@
             <a href='/stories' class="item">Mentions</a>
             <a href='/projects' class="item">Projects</a>
             <a href='/media' class="item">Media</a>
-            {{-- @if ( Auth::user()->hasRole('siteadmin') )
-                <a href="/clients" class="item">Clients</a>
-            @endif --}}
+            @if ( Auth::user()->hasRole('siteadmin') )
+               <a href="/clients">Clients</a>
+            @endif
         </div>
         <div class="pusher">
             <div class="topbar">
