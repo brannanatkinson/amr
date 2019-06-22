c980f331a0fb429c7c52ac51dc33fcc6c47e910d
Brannan Atkinson <brannan@amycommuniations.com>
2019-06-22 09:12:41 -0500

    testing signincontroller

--- app/Http/Controllers/SigninController.php
+++ app/Http/Controllers/SigninController.php
@@ -25,7 +25,6 @@ class SigninController extends Controller
         if ( User::where('email', '=', $request->email )->exists()) {
             $user = User::where('email', '=', $request->email)->first();
             $url = $user->signed_url;
-            dd($user);
             Mail::to($request->email)->send(new LoginLink($user, $url));
             $confirmation_details = [];
             $confirmation_details['email'] = $request->email;
