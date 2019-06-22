e08d48cfb8c5809f0d2dfc7646526b586bcecb96
Brannan Atkinson <brannan@amycommuniations.com>
2019-06-22 16:16:05 -0500

    created share controller and view for projects

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -10,7 +10,7 @@
 				
 				<div class="ui segment">
 				<p>Public URL for sharing. Copy and paste to share this project.</p>
-			    <pre>http://www.atkinsonmediareports.com/{{ $project->id }}/share</pre>
+			    <pre>http://www.atkinsonmediareports.com/projects/{{ $project->id }}/share</pre>
 			    </div>
 			    <hr>
 			@else 
