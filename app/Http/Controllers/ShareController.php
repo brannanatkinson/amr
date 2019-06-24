bdfe2460ec6620c554fd50c7666af51b014c6737
Brannan Atkinson <brannan@amycommuniations.com>
2019-06-24 13:30:03 -0500

    changed share URL to random string

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -12,6 +12,6 @@ class ShareController extends Controller
     public function share_project_url ($id){
         $project = Project::where('project_share_id', '=', $id)->first();
         $stories = Story::where('project_id', '=', $project->$id)->orderBy('story_date', 'desc')->simplePaginate(15);
-        return view('projects.show', compact('stories', 'project'));
+        return view('stories.index', compact('stories', 'project'));
     }
 }
