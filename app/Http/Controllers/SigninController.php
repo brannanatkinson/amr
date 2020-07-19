6b8330d528bd4b090fad0415dff0db3808a0fa34
Brannan Atkinson <brannan@amycommuniations.com>
2020-07-19 12:49:17 -0500

    fixing login

--- app/Http/Controllers/SigninController.php
+++ app/Http/Controllers/SigninController.php
@@ -63,9 +63,9 @@ class SigninController extends Controller
         //dd($users);
         $users->map( function ($user){
             dump($user);
-            $user->signed_url = URL::signedRoute('login', ['user' => $user->id]);
-            //$user->save();
-            //dump($user->signed_url);
+            $user->signed_url = URL::signedRoute('signin', ['user' => $user->id]);
+            $user->save();
+            dump($user->signed_url);
         });
     }
     
