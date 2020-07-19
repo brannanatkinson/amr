490c53ae5fc7c075f15ba759326f1e0504af9c01
Brannan Atkinson <brannan@amycommuniations.com>
2020-07-19 09:52:28 -0500

    fixing login

--- app/Mail/LoginLink.php
+++ app/Mail/LoginLink.php
@@ -37,7 +37,7 @@ class LoginLink extends Mailable
     public function build()
     {
         return $this->from(['address' => 'brannan@amyacommunications.com', 'name' => 'Brannan Atkinson'])->view('emails.loginlink')
-            ->with(['loginlink' => $this->loginlink])
-        ;
+            ->with(['loginlink' => $this->loginlink]);
+        dd('this');
     }
 }
