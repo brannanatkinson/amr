781433e608cf9bb6994e86616538d5708e4668fa
Brannan Atkinson <brannanatkinson@brannans-mbp-2.lan>
2020-12-18 15:17:18 -0600

    fixing media page

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -15,14 +15,14 @@
 			@foreach ($outlets as $outlet)
 				<a name="{{$outlet->id}}"></a>
 				<h2 style="font-weight: bolder; padding-top: 20px;">{{ $outlet->org_name }} <a class="ui label" style="float:right; " href="#top">Back to Top</a></h2> 
-				<div style="margin-top: -10px; " class="media__count">~ Mentions: {{ $outlet->stories(Auth::user()->client_id)->count() }} ~</div> 
+				<!-- <div style="margin-top: -10px; " class="media__count">~ Mentions: {{ $outlet->stories(Auth::user()->client_id)->count() }} ~</div> 
 				@if ($outlet->stories(Auth::user()->client_id)->count() > 0)
                     <ul>
 					@foreach ($outlet->stories(Auth::user()->client_id)->get() as $story)
 			        	<li style="margin-bottom: 8px;"><a href="{{ $story->story_url }}">{{ $story->headline() }}</a><br> <span>{{ \Carbon\Carbon::parse($story->story_date)->toFormattedDateString() }} -- {{ $story->project->project_name }}</span></li>
 					@endforeach
 					</ul>
-				@endif
+				@endif -->
 			@endforeach
 	</div>
 </div>
